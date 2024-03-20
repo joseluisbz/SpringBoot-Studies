aea5f56d880ea2fcd825d0296d7e1a57a7e7dcd2
Jose Luis Bernal Zambrano <joseluisbz@gmail.com>
2024-03-20 17:21:14 -0500

    initial ms-people project

---
+++ ms-people/src/test/java/org/bz/eggprice/mspeople/MsPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bz.eggprice.mspeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
