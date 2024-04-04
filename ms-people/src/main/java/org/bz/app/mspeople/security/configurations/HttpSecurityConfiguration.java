84f09e64371c68a81ee7a04473e077b74db82e47
Jose Luis Bernal Zambrano <joseluisbz@gmail.com>
2024-04-03 23:29:57 -0500

    Se agrega clase para inhabilitar el Security cuando no se usa.

---
+++ ms-people/src/main/java/org/bz/app/mspeople/security/configurations/HttpSecurityConfiguration.java
@@ -0,0 +1,18 @@
+package org.bz.app.mspeople.security.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class HttpSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
