bbd4cd11a2e1402b4b90ecb40df7d9770c1cadb1
Jose Luis Bernal Zambrano <joseluisbz@gmail.com>
2024-04-11 21:34:46 -0500

    Disabling Code Execution in Testing

--- ms-people/src/main/java/org/bz/app/mspeople/configurations/SpringRootConfig.java
+++ ms-people/src/main/java/org/bz/app/mspeople/configurations/SpringRootConfig.java
@@ -3,30 +3,35 @@ package org.bz.app.mspeople.configurations;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.hsqldb.util.DatabaseManagerSwing;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
 
 @RequiredArgsConstructor
+@Component(value = "customSpringRootConfig")
 @Configuration
 public class SpringRootConfig {
 
     private final DataSource dataSource;
+
+    @Value("${only.for.testing:#{null}}")
+    private String onlyForTesting;
 
     @Bean
     public JdbcTemplate getJdbcTemplate() {
         return new JdbcTemplate(dataSource);
     }
 
-    //default username : sa, password : ''
     @PostConstruct
     public void getDbManager() {
-        DatabaseManagerSwing.main(
-                new String[]{"--url", "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", ""});
-
+        if (onlyForTesting == null) {
+            DatabaseManagerSwing.main(new String[]{"--url", "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", ""});
+        }
     }
 
 }
