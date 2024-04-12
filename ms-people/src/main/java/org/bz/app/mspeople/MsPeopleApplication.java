309b294116990543fc15ef54daef0bd3f2dd85e3
Jose Luis Bernal Zambrano <joseluisbz@gmail.com>
2024-04-12 00:21:17 -0500

    List All Bean Names

--- ms-people/src/main/java/org/bz/app/mspeople/MsPeopleApplication.java
+++ ms-people/src/main/java/org/bz/app/mspeople/MsPeopleApplication.java
@@ -4,14 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
 public class MsPeopleApplication {
+    private static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        SpringApplication.run(MsPeopleApplication.class, args);
+        applicationContext = SpringApplication.run(MsPeopleApplication.class, args);
+        displayAllBeans();
     }
 
     @Bean
@@ -19,6 +22,13 @@ public class MsPeopleApplication {
         return args -> {
             log.info("CommandLineRunner del Microservicio.");
         };
+    }
+
+    public static void displayAllBeans() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : allBeanNames) {
+            log.info("beanName: " + beanName);
+        }
     }
 
 }
