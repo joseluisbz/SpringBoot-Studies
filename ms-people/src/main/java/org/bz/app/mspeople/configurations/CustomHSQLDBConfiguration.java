98d352b03582cadc0c1674d410ea51e220edbb58
Jose Luis Bernal Zambrano <joseluisbz@gmail.com>
2024-04-19 11:06:51 -0500

    Refactor CustomHSQLDBConfiguration, remove DataSourceConfiguration

--- ms-people/src/main/java/org/bz/app/mspeople/configurations/CustomHSQLDBConfiguration.java
+++ ms-people/src/main/java/org/bz/app/mspeople/configurations/CustomHSQLDBConfiguration.java
@@ -1,28 +1,37 @@
 package org.bz.app.mspeople.configurations;
 
 import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.hsqldb.util.DatabaseManagerSwing;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
 
-@RequiredArgsConstructor
 @Configuration(value = "customHSQLDBConfiguration")
 public class CustomHSQLDBConfiguration {
-
-    private final DataSource dataSource;
 
     @Value("${only.for.testing:#{null}}")
     private String onlyForTesting;
 
     @Bean
+    public DataSource dataSource() {
+        //jdbc:hsqldb:mem:testdb
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL)
+                //.addScript("db/hsqldb/db.sql")
+                .build();
+        return db;
+    }
+
+    @Bean
     public JdbcTemplate getJdbcTemplate() {
-        return new JdbcTemplate(dataSource);
+        return new JdbcTemplate(dataSource());
     }
 
     @PostConstruct
